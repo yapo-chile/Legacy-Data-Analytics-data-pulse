d902ee8ffb08ccaeb68547bdbf6548f67ca820ac
Luis Barrera <luis.barrera@schibsted.com>
2020-01-13 16:20:13 -0300

    [feat/buyer-return]: Add global metric buyer return over past and current.

---
+++ buyer-return/app/infraestructure/psql.py
@@ -0,0 +1,139 @@
+import logging
+import psycopg2
+import psycopg2.extras
+import pandas as pd
+
+
+class Database:
+    """
+    Class that allow do operations with postgresql database.
+    """
+    def __init__(self, conf):
+        self.log = logging.getLogger('psql')
+        date_format = """%(asctime)s,%(msecs)d %(levelname)-2s """
+        info_format = """[%(filename)s:%(lineno)d] %(message)s"""
+        log_format = date_format + info_format
+        logging.basicConfig(format=log_format, level=logging.INFO)
+        self.conf = conf
+        self.connection = None
+        self.get_connection()
+
+    def database_conf(self) -> dict:
+        """
+        Method that return dict with database credentials.
+        """
+        return {"host": self.conf.host,
+                "port": self.conf.port,
+                "user": self.conf.user,
+                "password": self.conf.password,
+                "dbname": self.conf.name}
+
+    def get_connection(self) -> None:
+        """
+        Method that returns database connection.
+        """
+        self.log.info('get_connection DB %s/%s', self.conf.host, self.conf.name)
+        self.connection = psycopg2.connect(**self.database_conf())
+        self.connection.set_client_encoding('UTF-8')
+
+    def execute_command(self, command) -> None:
+        """
+        Method that allow execute sql commands such as DML commands.
+        """
+        self.log.info('execute_command : %s',
+                      command.replace('\n', ' ').replace('\t', ' '))
+        cursor = self.connection.cursor()
+        cursor.execute(command)
+        self.connection.commit()
+        cursor.close()
+
+    def select_to_dict(self, query) -> pd.DataFrame:
+        """
+        Method that from query transform raw data into dict.
+        """
+        self.log.info('Query : %s', query.replace(
+            '\n', ' ').replace('    ', ' '))
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        fieldnames = [name[0] for name in cursor.description]
+        result = []
+        for row in cursor.fetchall():
+            rowset = []
+            for field in zip(fieldnames, row):
+                rowset.append(field)
+            result.append(dict(rowset))
+        pd_result = pd.DataFrame(result)
+        cursor.close()
+        return pd_result
+
+    def insert_data_current(self, data_dict: pd.DataFrame) -> None:
+        self.log.info('INSERT INTO %s', self.conf.table_current)
+        page_size: int = 10000
+        with self.connection.cursor() as cursor:
+            psycopg2.extras \
+                .execute_values(cursor,
+                                """ INSERT INTO """ + self.conf.table_current +
+                                """ ( dt_metric,
+                                      first_period_start,
+                                      first_period_end,
+                                      second_period,
+                                      buyers_first_period,
+                                      buyers_second_period,
+                                      buyers_both_periods
+                                    )
+                                    VALUES %s; """, ((
+                                        row.dt_metric,
+                                        row.first_period_start,
+                                        row.first_period_end,
+                                        row.second_period,
+                                        row.buyers_first_period,
+                                        row.buyers_second_period,
+                                        row.buyers_both_periods
+                                    ) for row in data_dict.itertuples()),
+                                page_size=page_size)
+            self.log.info('INSERT %s COMMIT.', self.conf.table_current)
+            self.connection.commit()
+            self.log.info('CLOSE CURSOR %s', self.conf.table_current)
+            cursor.close()
+
+    def insert_data_past(self, data_dict: pd.DataFrame) -> None:
+        self.log.info('INSERT INTO %s', self.conf.table_past)
+        page_size: int = 10000
+        with self.connection.cursor() as cursor:
+            psycopg2.extras \
+                .execute_values(cursor,
+                                """ INSERT INTO """ + self.conf.table_past +
+                                """ ( dt_metric,
+                                      first_period_start,
+                                      first_period_end,
+                                      second_period_start,
+                                      second_period_end,
+                                      buyers_first_period,
+                                      buyers_second_period,
+                                      buyers_both_periods
+                                    )
+                                    VALUES %s; """, ((
+                                        row.dt_metric,
+                                        row.first_period_start,
+                                        row.first_period_end,
+                                        row.second_period_start,
+                                        row.second_period_end,
+                                        row.buyers_first_period,
+                                        row.buyers_second_period,
+                                        row.buyers_both_periods
+                                    ) for row in data_dict.itertuples()),
+                                page_size=page_size)
+            self.log.info('INSERT %s COMMIT.', self.conf.table_past)
+            self.connection.commit()
+            self.log.info('CLOSE CURSOR %s', self.conf.table_past)
+            cursor.close()
+
+
+    def close_connection(self):
+        """
+        Method that close connection to postgresql database.
+        """
+        self.log.info('Close connection DB : %s/%s',
+                      self.conf.host,
+                      self.conf.name)
+        self.connection.close()
