812b9f508e1ca479d0cc3e110c53e46dafb169ce
Luis Barrera <luis.barrera@schibsted.com>
2020-01-13 15:12:09 -0300

    [feat/bounce-rate-arch]: Standard APPNAME docker image from data team.

--- bounce-rate/scripts/commands/vars.mk
+++ bounce-rate/scripts/commands/vars.mk
@@ -16,7 +16,7 @@ export REPORT_ARTIFACTS=reports
 #APP variables
 genport = $(shell expr \( $(shell id -u) - \( $(shell id -u) / 100 \) \* 100 \) \* 200 + 30400 + $(1))
 
-export APPNAME=dp-bounce-rate
+export APPNAME=data-pulse-bounce-rate
 export VERSION=0.0.1
 export EXEC=./${APPNAME}
 export YO=$(shell expr `whoami`)
