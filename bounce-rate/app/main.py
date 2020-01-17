b0f54790d7719a1c81d99f4af471359ea982ea47
Luis Barrera <luis.barrera@schibsted.com>
2020-01-17 13:34:46 -0300

    [feat/bounce-rate-arch]: Modularize SOURCE and WRITE data.

--- bounce-rate/app/main.py
+++ bounce-rate/app/main.py
@@ -9,6 +9,25 @@ from utils.query import Query
 from utils.read_params import ReadParams
 from utils.time_execution import TimeExecution
 
+# Query data from Pulse bucket
+def source_data_pulse(params: ReadParams,
+                      config: getConf):
+    athena = Athena(conf=CONFIG.athenaConf)
+    query = Query()
+    data_athena = athena.get_data(query.query_bounce_rate(PARAMS))
+    athena.close_connection()
+    return data_athena
+
+# Write data to data warehouse
+def write_data_dwh(params: ReadParams,
+                   config: getConf,
+                   data_athena: pd.DataFrame) -> None:
+    query = Query()
+    DB_WRITE = Database(conf=config.db)
+    DB_WRITE.execute_command(query.delete_bounce_rate(PARAMS))
+    DB_WRITE.insert_data(data_athena)
+    DB_WRITE.close_connection()
+
 
 if __name__ == '__main__':
     CONFIG = getConf()
@@ -19,13 +38,7 @@ if __name__ == '__main__':
     LOG_FORMAT = DATE_FORMAT + INFO_FORMAT
     logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
     PARAMS = ReadParams(sys.argv)
-    ATHENA = Athena(conf=CONFIG.athenaConf)
-    QUERY = Query()
-    DATA = ATHENA.get_data(QUERY.query_bounce_rate(PARAMS))
-    ATHENA.close_connection()
-    DB_WRITE = Database(conf=CONFIG.db)
-    DB_WRITE.execute_command(QUERY.delete_bounce_rate(PARAMS))
-    DB_WRITE.insert_data(DATA)
-    DB_WRITE.close_connection()
+    DATA_ATHENA = source_data_pulse(PARAMS, CONFIG)
+    write_data_dwh = write_data_dwh(PARAMS, CONFIG, DATA_ATHENA)
     TIME.get_time()
     LOGGER.info('Process ended successfully.')
