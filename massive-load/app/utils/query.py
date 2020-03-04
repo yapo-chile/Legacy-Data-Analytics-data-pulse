1d9da43225a3d72a5cc958815ffb79a42158e321
Luis Barrera <luis.barrera@schibsted.com>
2020-03-04 09:34:01 -0300

    [feat/massive-load]: Fix pyAthena specific version and fix query

--- massive-load/app/utils/query.py
+++ massive-load/app/utils/query.py
@@ -27,9 +27,23 @@ class Query:
         """
         Method return str with query
         """
-        query = """
-        select '1'
-        """
+        query =  """
+        select
+            cast(date_parse(cast(year as varchar) || '-' || cast(month as varchar) || '-' || cast(day as varchar),'%Y-%c-%e') as date) timedate,
+            split_part(ad_id,':',4) list_id,
+            count(distinct case when event_type = 'View' and object_type = 'ClassifiedAd' then event_id end) number_of_views,
+            count(distinct case when event_type = 'Call' then event_id end) number_of_calls,
+            count(distinct case when event_type = 'Show' then event_id end) number_of_show_phone,
+            count(distinct case when event_type = 'Send' then environment_id end) number_of_ad_replies
+        from
+            yapocl_databox.insights_events_behavioral_fact_layer_365d
+        where
+            cast(date_parse(cast(year as varchar) || '-' || cast(month as varchar) || '-' || cast(day as varchar),'%Y-%c-%e') as date) = date '{0}'
+            and ad_id != 'sdrn:yapocl:classified:' and ad_id != 'sdrn:yapocl:classified:0'
+            and local_main_category in ('inmuebles','vehiculos','vehículos','veh韈ulos','veh�culos')
+        group by 1,2
+        order by 1,2
+        """.format(self.params.get_date_from())
         return query
 
     def delete_base(self) -> str:
